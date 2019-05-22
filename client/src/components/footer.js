4c19c49cdfa631b24a35f6345810657805851447
Glitch (rigorous-laugh) <none>
2019-05-22 23:14:21 +0000

    👙🌴 Updated footer

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -5,7 +5,7 @@ import './footer.css';
 const Footer = () => {  
   return (
     <div id="footer">
-      <div className="footerContent">Designed by <a target="_blank" href="https://codepen.io/atiyahaider/">Atiya Haider</a></div>      
+      <div className="footerContent">Designed by <a target="_blank" href="https://s.codepen.io/atiyahaider/debug/oaZxeb/dGrXWdOKgPWM">Atiya Haider</a></div>      
       <Dropdown />
     </div>
   );
